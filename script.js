7efd6ad63992a961dec65bccd829545d1a1c7aba
antonio <tomazini.antonio@escola.pr.gov.br>
2025-08-11 11:25:05 -0300

    finalmente

---
+++ script.js
@@ -0,0 +1,40 @@
+// ==================================================
+// // ==> SELECIONAR ELEMENTOS <==
+// 1. Selecione o elemento <body> e armazene em uma variável
+let body = 
+
+// 2. Selecione todos os elementos <p> e armazene em uma variável
+let paragrafos = 
+
+// 3. Selecione o elemento com id "meuId" (crie este elemento no HTML se necessário)
+let elementoPorId = 
+
+// ==================================================
+// ==> CRIAR E ADICIONAR ELEMENTOS
+// 4. Crie um novo elemento <h1>
+let novoH1 = 
+
+// 5. Defina o texto do h1 como "Minha Atividade DOM"
+novoH1.textContent = 
+
+// 6. Adicione o h1 ao body
+
+// ==================================================
+// ==> MODIFICAR ESTILOS <==
+// 7. Mude a cor de fundo do body para "lightblue"
+body.style.
+
+/*
+// 8. Mude a cor do texto de todos os parágrafos para "darkgray"
+for (let p of paragrafos) {
+  p.style.
+}
+
+// ==================================================
+// ==> MANIPULAR ATRIBUTOS <==
+// 9. Adicione uma classe "destaque" ao elemento com id "meuId"
+docucumet.getElementByid("meuid").classlist.add(destaque)
+
+// 10. Adicione um atributo "title" com valor "Elemento destacado" ao mesmo elemento
+documet.getElementByid("meuid").seTAttbute("title", )
+*/
